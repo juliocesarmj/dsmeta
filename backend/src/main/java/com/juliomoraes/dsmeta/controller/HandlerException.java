fa4318ce137e8a8177b6cf5d3badd455bb25441e
Julio Moraes <juliocesardemoraes2015@gmail.com>
2022-07-16 11:04:25 -0300

    update message exception

--- backend/src/main/java/com/juliomoraes/dsmeta/controller/HandlerException.java
+++ backend/src/main/java/com/juliomoraes/dsmeta/controller/HandlerException.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,13 +15,13 @@ import com.juliomoraes.dsmeta.service.exceptions.SaleExceptionNotFound;
 public class HandlerException {
 	
 	@ExceptionHandler(SaleExceptionNotFound.class)
-	private ErrorExceptionHandler handlerStandardException(SaleExceptionNotFound s, HttpServletRequest r) {
+	private ResponseEntity<ErrorExceptionHandler> handlerStandardException(SaleExceptionNotFound s, HttpServletRequest r) {
 		ErrorExceptionHandler exception = new ErrorExceptionHandler();
 		exception.setTimestamp(LocalDate.now());
 		exception.setStatus(HttpStatus.SC_NOT_FOUND);
-		exception.setError("Venda não encontrada.");
+		exception.setError(s.getMessage());
 		exception.setPath(r.getRequestURI());
 		
-		return exception;
+		return ResponseEntity.status(org.springframework.http.HttpStatus.NOT_FOUND).body(exception);
 	}
 }
